6fc7f50f820ec615551d27b9b0c3f3ce6fe83dc3
Yeonwoo Sung <neos960518@gmail.com>
2022-05-20 14:49:51 +0900

    Create empty_trash_bin.py

---
+++ python/empty_trash_bin.py
@@ -0,0 +1,23 @@
+def empty_trash():
+    """Empty trash folder.
+
+    """
+    text = "[tl] Empty the trash"
+    if _platform == "linux" or _platform == "linux2":
+        print('linux: %s' % text)
+        os.system("rm -rf ~/.local/share/Trash/*")
+    elif _platform == "darwin":
+        print('OS X: %s' % text)
+        os.system("sudo rm -rf ~/.Trash/*")
+    elif _platform == "win32":
+        print('Windows: %s' % text)
+        try:
+            os.system("rd /s c:\$Recycle.Bin")  # Windows 7 or Server 2008
+        except:
+            pass
+        try:
+            os.system("rd /s c:\recycler")  #  Windows XP, Vista, or Server 2003
+        except:
+            pass
+    else:
+        print(_platform)
