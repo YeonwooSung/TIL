155e6d62dbdd24e466f899af3e68b3998cdbb680
YeonwooSung <neos960518@gmail.com>
2022-11-07 13:34:53 +0900

    Add example for format string

---
+++ python/string/format_print.py
@@ -0,0 +1,13 @@
+text = 'EPIC'
+
+print(f'{text}')
+# output > EPIC
+
+print(f'{text:#>10}')
+# output > #######EPIC
+
+print(f'{text:#^10}')
+# output > ###EPIC###
+
+print(f'{text:#<10}')
+# output > EPIC######
