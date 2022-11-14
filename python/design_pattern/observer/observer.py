5f8968d757bbdaa1b670c4176203ec9b6d90163a
YeonwooSung <neos960518@gmail.com>
2022-11-14 15:36:15 +0900

    Test commit

--- python/design_pattern/observer/observer.py
+++ python/design_pattern/observer/observer.py
@@ -19,6 +19,7 @@ class SubjectABC(ABC):
 
 class Grepper(SubjectABC):
     def __init__(self):
+        #TODO
         self._observers: list[ObserverABC] = []
 
     def register(self, observer: ObserverABC):
