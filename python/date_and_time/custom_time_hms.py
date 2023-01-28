16786a8485dcd0ead5d963327ba0223cbcb0562f
YeonwooSung <neos960518@gmail.com>
2023-01-28 15:39:12 +0900

    Add example codes for handling datetime and timezone

---
+++ python/date_and_time/custom_time_hms.py
@@ -0,0 +1,8 @@
+from datetime import time
+
+a = time(11, 34, 56)
+
+print("Hour =", a.hour)
+print("Minute =", a.minute)
+print("Second =", a.second)
+print("Microsecond =", a.microsecond)
---
+++ python/date_and_time/datetime_obj.py
@@ -0,0 +1,6 @@
+import datetime
+
+d = datetime.date(2022, 12, 25)
+print(d)
+
+print('Attributes of datetime obj: ', dir(datetime))
